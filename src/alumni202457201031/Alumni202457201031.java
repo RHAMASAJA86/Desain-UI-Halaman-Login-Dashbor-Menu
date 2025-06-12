a7a0a296c2d4601b695f05fdd0ed70d162004e3e
ubaydillah <m.ubaiydillah.r@gmail.com>
2025-06-12 15:25:57 +0700

    commit1

---
+++ src/alumni202457201031/Alumni202457201031.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alumni202457201031;
+
+/**
+ *
+ * @author asus
+ */
+public class Alumni202457201031 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
